0d2305f73b2e31542d8e52223d263a9ada28e731
Edson Mallet <edsonmmr@gmail.com>
2026-07-27 10:31:17 -0300

    feat(bff): register /v1/super/tenants routes for super_admin

--- apps/bff/src/infrastructure/http/container.ts
+++ apps/bff/src/infrastructure/http/container.ts
@@ -1,4 +1,7 @@
-import { createTenantAuthPreHandler } from '@clube/fastify-plugins'
+import {
+  createFirebaseAuthPreHandler,
+  createTenantAuthPreHandler,
+} from '@clube/fastify-plugins'
 import { AddDomainUseCase } from '../../application/tenants/add-domain.usecase'
 import { GetLandingConfigUseCase } from '../../application/tenants/get-landing-config.usecase'
 import { GetMeUseCase } from '../../application/tenants/get-me.usecase'
@@ -6,6 +9,11 @@ import { ListDomainsUseCase } from '../../application/tenants/list-domains.useca
 import { RegisterUserUseCase } from '../../application/tenants/register-user.usecase'
 import { UpdateLandingConfigUseCase } from '../../application/tenants/update-landing-config.usecase'
 import { VerifyDomainUseCase } from '../../application/tenants/verify-domain.usecase'
+import { CreateTenantUseCase } from '../../application/super-tenants/create-tenant.usecase'
+import { GetTenantUseCase } from '../../application/super-tenants/get-tenant.usecase'
+import { ImpersonateTenantUseCase } from '../../application/super-tenants/impersonate-tenant.usecase'
+import { ListTenantsUseCase } from '../../application/super-tenants/list-tenants.usecase'
+import { UpdateTenantStatusUseCase } from '../../application/super-tenants/update-tenant-status.usecase'
 import { db } from '../db'
 import { DomainRepository } from '../db/repositories/domain.repository'
 import { LandingConfigRepository } from '../db/repositories/landing-config.repository'
@@ -33,3 +41,11 @@ export const updateLandingConfigUseCase = new UpdateLandingConfigUseCase(
 export const listDomainsUseCase = new ListDomainsUseCase(domainRepository)
 export const addDomainUseCase = new AddDomainUseCase(domainRepository)
 export const verifyDomainUseCase = new VerifyDomainUseCase(domainRepository)
+
+export const superAuthPreHandler = createFirebaseAuthPreHandler()
+
+export const listTenantsUseCase = new ListTenantsUseCase(tenantRepository)
+export const getTenantUseCase = new GetTenantUseCase(tenantRepository)
+export const createTenantUseCase = new CreateTenantUseCase(tenantRepository, userRepository)
+export const updateTenantStatusUseCase = new UpdateTenantStatusUseCase(tenantRepository)
+export const impersonateTenantUseCase = new ImpersonateTenantUseCase(tenantRepository)
